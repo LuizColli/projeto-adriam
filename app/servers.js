caf55894cd1d472865e43fd7e65f5e1a5c75f603
LuizColli <luizgcolli@gmail.com>
2025-11-07 16:04:28 -0300

    first commit

---
+++ app/servers.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const client = require('prom-client');
+
+const app = express();
+
+client.collectDefaultMetrics();
+
+const counter = new client.Counter({
+    name: "app_request_total",
+    help: "contador de requisições recebidas"
+});
+
+app.get('/', (req, res) => {
+    counter.inc();
+    res.send("Prometheus+Grafana+Kubernetes");
+});
+
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', client.register.contentType);
+    res.end(await client.register.metrics());
+});
+
+app.listen(3000, () => {
+    console.log('App running on port 3000');
+});
